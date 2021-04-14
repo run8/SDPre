1c43fecb30dbb1df9a5dac3867519e6dabb06157
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-04-14 16:54:57 -0700

    Submit Obj2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+   System.out.println("Name: " + name + "\nAge: " + age + "\nPOB: " + state);
+    // System.out.println("Hi \n there");
+  }
+}
