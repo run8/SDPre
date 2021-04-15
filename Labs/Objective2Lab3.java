7c37ecdcc2825f0343fb2338f45b5a5c5d7c1fc0
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-04-14 17:02:09 -0700

    Submitt Obj2 Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 3.5;
+    num2 = 2;
+    num3 = 4.5;
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
