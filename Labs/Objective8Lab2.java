506512aa7b8afa0aa457785d921f1448329fdf8d
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-04-14 21:13:41 -0700

    Complete Obj8 Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int i =0; i<21; i+=1){
+      sum += i;
+    }
+
+    System.out.println(sum);
+
+  }
+}
