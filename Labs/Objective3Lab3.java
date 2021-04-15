280448da74876b6edb1c5ca5e42f9d49141f02be
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-04-14 18:18:46 -0700

    Complete Obj3 Lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,9 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    int first, second, third;
+    first = 1 + (int)(Math.random() * 6);
+    second = 1 + (int)(Math.random() * 6);
+    third = 1 + (int)(Math.random() * 6);
+    System.out.println("Random num1:" + first +"\nRandom num2: " + second + "\nRandom num3: " + third);
+  }
+}
