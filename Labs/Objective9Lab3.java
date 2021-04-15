17fbc48e82f410b4441f624c936d62d9a5e0ac5a
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-04-14 22:58:00 -0700

    Complete Obj9 Lab3

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Objective9Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+      printMenu();
+      selection = scanner.nextInt();
+      if (selection == 1) {
+        printMenu();
+      } else if (selection == 2){
+        favoriteFood();
+      } else if (selection == 3){
+        System.out.println("Goodbye");
+        break;
+      } else {
+        printMenu();
+      }
+    }
+
+    scanner.close();
+
+  }
+
+  public static void sayHello() {
+    System.out.println("Hello Human");
+
+  }
+
+  public static void favoriteFood(){
+    System.out.println("Apples, Bananas, Coconuts");
+  }
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+}
