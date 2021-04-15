a6153c5aa4591175c590e9309e0305dcb61ef6be
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-04-14 20:40:01 -0700

    Complete Obj7 Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int sum = 0;
+    int counter = 0;
+
+    while (counter < 21) {
+      sum += counter;
+      counter += 1;
+    }
+    System.out.println(sum);
+  }
+}
